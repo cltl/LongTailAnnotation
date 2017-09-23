423464ae72d863e4c59de6d195efa1e577f6bce8
Filip Ilievski <filip.dbrsk@gmail.com>
2017-09-23 17:42:37 +0000

    disqualify docs

--- client/static/annotations.js
+++ client/static/annotations.js
@@ -6,11 +6,11 @@ $(function(){
         var new_inc = unsorted['new'];
         var old_sorted = old_inc.sort();
         var new_sorted = new_inc.sort();
-        $('#pickfile').append($('<option></option>').val('-1').html("--INCIDENTS YOU'VE WORKED ON--"));
+        $('#pickfile').append($('<option></option>').val('-1').html("--INCIDENTS YOU'VE WORKED ON--").prop('disabled', true));
         for(var i = 0; i < old_sorted.length; i++) {
             $('#pickfile').append($('<option></option>').val(old_sorted[i]).html(old_sorted[i]));
         }
-        $('#pickfile').append($('<option></option>').val('-1').html("--OTHER INCIDENTS--"));
+        $('#pickfile').append($('<option></option>').val('-1').html("--OTHER INCIDENTS--").prop('disabled', true));
         for(var i = 0; i < new_sorted.length; i++) {
             $('#pickfile').append($('<option></option>').val(new_sorted[i]).html(new_sorted[i]));
         }
